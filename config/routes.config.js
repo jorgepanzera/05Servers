ab48b730b6b622169f5a754a9ab690d7e7868006
Jorge <panzera.jorge@gmail.com>
2023-04-27 21:18:19 -0300

    All methods and postman

--- config/routes.config.js
+++ config/routes.config.js
@@ -4,7 +4,9 @@ const  controller = require ('../controllers/employee.controller.js')
 const router = Router();
 
 // Routes here
-router.get("/employees", controller.getAllEmployees)
+router.get("/employees", controller.getEmployees)
+router.get("/employees/oldest", controller.getOldestEmployee)
+router.get("/employees/:name", controller.getEmployeesByName)
 router.post("/employees", controller.createEmployee)
 
 module.exports = router;
--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -1,8 +1,42 @@
 
 const employees = require("../models/employees.json")
 
-const getAllEmployees = (req,res) => {
+const getEmployees = (req, res) => {
+  
+  const page = req.query.page || 0;
+  const user = req.query.user || false;
+  const badges = req.query.badges || null;
+
+  if (page > 0) {
+    // Devolverá del elemento (2 * (N - 1)) al (2 * (N - 1)) + 1
+    const employeesPerPage = 2
+    const startIndex = employeesPerPage * (page - 1)
+    const endIndex = (employeesPerPage * (page - 1)) + 1
+    const pageEmployees = employees.slice(startIndex, endIndex+1);
+    return res.json(pageEmployees);
+  }
+
+  if (user) {
+    // Devolverá listado de employees con privileges == "user"
+    const filteredEmployess = employees.filter(employee => employee.privileges==="user")
+    return res.json(filteredEmployess);
+  }
+
+  if (badges !== null) {
+    //Devolverá listado de employees que incluya "black" en el atributo "badges"
+    const filteredEmployess = employees.filter(employee => employee.badges.includes(badges))
+    return res.json(filteredEmployess);
+  }
+
+  // Todos los employees
   return res.json(employees)
+  
+}
+
+const getOldestEmployee = (req, res) => { 
+  
+  const oldestEmployees = employees.sort((a, b) => b.age - a.age)
+  return res.json(oldestEmployees[0])
 }
 
 const createEmployee =  (req, res) => {
@@ -15,12 +49,27 @@ const createEmployee =  (req, res) => {
     }
   
     // Agregar al array
-    push(req.body);
+    employees.push(req.body)
   
     // Devolver lista con todos los employees
-    return res.status(201).send(employees)
+    return res.status(201).send("Employee Created")
 
+}
+  
+const getEmployeesByName = (req, res) => { 
+  
+  const name = req.params.name || false
+  
+  if (name) {
+    const filteredEmployess = employees.filter(employee => employee.name === name)
+    if (filteredEmployess.length > 0) {
+      return res.json(filteredEmployess);
+    } 
   }
 
-  module.exports = { getAllEmployees, createEmployee }
+  return res.status(404).send("Employee not found")
+
+}
+
+  module.exports = { getEmployees, getOldestEmployee, createEmployee, getEmployeesByName }
   
